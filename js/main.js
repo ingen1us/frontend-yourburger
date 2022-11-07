49528d9aace06da5b81e52cea129f24473386594
ingen1us <yeinernaranjo@gmail.com>
2022-11-07 17:36:08 -0500

    Hechas las validaciones de los formularios en Usuarios y Roles

--- js/main.js
+++ js/main.js
@@ -21,11 +21,15 @@ function toggleMenu() {
 let isActive = false;
 let activeForm;
 let activeInputs;
+let activeName;
 let allButtons = document.querySelectorAll("button");
+let hayEmail = false;
+let haySlides = false;
 
 allButtons.forEach((btn) => {
   btn.onclick = function () {
     if (this.name != "") {
+      activeName = this.name;
       activeForm = document.querySelector(`.form-${this.name}`);
       activeInputs = document.querySelectorAll(`.form-${this.name} input`);
     }
@@ -46,17 +50,32 @@ allButtons.forEach((btn) => {
       } else {
         document.querySelector(`.modal-${btn.name}`).classList.add("is-active");
         isActive = true;
+        activeInputs.forEach((inp) => {
+          if (inp.type == "email") {
+            hayEmail = true;
+          }
+          if (document.querySelector("div.slides")) {
+            haySlides = true;
+          }
+        });
+        if (!hayEmail) {
+          campos["email"] = true;
+        }
+        if (!haySlides) {
+          campos["checkbox"] = true;
+        }
       }
     }
 
     activeInputs.forEach((input) => {
       input.addEventListener("keyup", validarFormulario);
       input.addEventListener("blur", validarFormulario);
+      input.addEventListener("change", validarFormulario);
     });
 
     activeForm.addEventListener("submit", (e) => {
       e.preventDefault();
-      if (campos.text && campos.email) {
+      if (campos.text && campos.email && campos.checkbox) {
         toggleForm();
       } else {
       }
@@ -80,6 +99,7 @@ const expresiones = {
 const campos = {
   text: false,
   email: false,
+  checkbox: false,
 };
 
 const validarFormulario = (e) => {
@@ -90,6 +110,25 @@ const validarFormulario = (e) => {
     case "email":
       validarCampo(expresiones.correo, e.target, "email");
       break;
+    case "checkbox":
+      validarSlides(e.target, "checkbox");
+  }
+};
+
+const validarSlides = (slide, campo) => {
+  let slides = document.querySelectorAll(
+    `.form-${activeName} .slides input[type=checkbox]`
+  );
+  let checkeados = 0;
+  slides.forEach((sl) => {
+    if (sl.checked) {
+      checkeados++;
+    }
+  });
+  if (checkeados >= 1) {
+    campos[campo] = true;
+  } else {
+    campos[campo] = false;
   }
 };
 
